a36c66c6b3ec7753d060a171033f2f54982bee17
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-24 20:43:30 -0400

    Update after_success.sh

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -5,17 +5,21 @@ set -ev
 git config user.name "Travis CI"
 git config user.email ""
 
-URL="$(echo ${TRAVIS_REPO_SLUG} | cut -d '/' -f 2)/index.html"
+if [ -z "${CRATE_NAME}" ]; then
+  CRATE_NAME=$(echo ${TRAVIS_REPO_SLUG} | cut -d '/' -f 2 | sed 's/-/_/g')
+fi
+
+URL="${CRATE_NAME}/index.html"
 
 HTML="<!DOCTYPE html>
 <link rel='canonical' href='${URL}'>
 <meta http-equiv='refresh' content='0; url=${URL}'>
 <script>window.location='${URL}'</script>"
 
-cargo doc
+cargo doc ${CARGO_FLAGS}
 echo "${HTML}" > target/doc/index.html
 
-sudo pip install ghp-import
-ghp-import -m 'Update the documentation' -n target/doc
+pip install ghp-import --user $USER
+$HOME/.local/bin/ghp-import -m 'Update the documentation' -n target/doc
 
 git push -qf https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
