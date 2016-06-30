68d1c03ee9acf98a4db10e0a4b191a5483bd966e
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-30 10:20:03 +0200

    Add a couple of guards to documentation.sh

--- travis/documentation.sh
+++ travis/documentation.sh
@@ -2,6 +2,10 @@
 
 set -ev
 
+[ ! -z "${RUSTDOC_VERSION}" ] && [ "${TRAVIS_RUST_VERSION}" != "${RUSTDOC_VERSION}" ] && exit
+[ -z "${RUSTDOC_VERSION}" ] && [ "${TRAVIS_RUST_VERSION}" != "nightly" ] && exit
+[ "${TRAVIS_BRANCH}" != "master" ] && exit
+
 git config user.name "Travis CI"
 git config user.email ""
 
