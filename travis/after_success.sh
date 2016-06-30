41c35e810aa73e400e6c418953731e25da3a837f
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-30 10:19:19 +0200

    Rename update_documentation.sh to documentation.sh

--- travis/after_success.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-if [ ! -z "${RUSTDOC_VERSION}" ] && [ "${RUSTDOC_VERSION}" != "${TRAVIS_RUST_VERSION}" ]; then
-  exit
-fi
-
-curl https://bodoni.github.io/travis/update_documentation.sh | bash
